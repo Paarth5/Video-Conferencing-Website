52621b08d3a031b720b96b40b2eaabf50e032873
Paarth <parthbansal2047@gmail.com>
2023-10-04 15:23:31 +0530

    Added Responsive Styles

--- middleware.js
+++ middleware.js
@@ -1,6 +1,12 @@
-const passport = require("passport");
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
     res.redirect("/login");
   } else next();
+};
+module.exports.storeReturnTo = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
+  }
+  next();
 };
